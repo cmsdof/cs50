1e49b38889578fe2dfe4d7fabc9adb9a91fe6e64
Carlos Severo <carlosmseverof@gmail.com>
2026-05-25 13:48:03 -0300

    feat(encryption): caesar cypher the input

--- lecture2-arrays/ceasar.c
+++ lecture2-arrays/ceasar.c
@@ -2,41 +2,88 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
+#include <stdlib.h>
 
+bool only_digits(string key);
+char rotate(char letter, int key);
 
 int main(int argc, string argv[])
 {
+    int key;
     // Make sure program was run with just one command-line argument
     if (argc > 2 || argc == 1)
     {
         printf("Usage: ./caesar key\n");
-        return 0;
+        return 1;
     }
 
     // Make sure every character in argv[1] is a digit
-    for (int i = 0; i < strlen(argv[1]); i++)
+    if (only_digits(argv[1]))
     {
-        if (isdigit(argv[1][i]) == 0)
-        {
-            printf("Usage: ./caesar key\n");
-            return 0;
-        }
+        // Convert argv[1] from a `string` to an `int`
+        key = atoi(argv[1]);
+    } else
+    {
+        return 1;
     }
-
-    // Convert argv[1] from a `string` to an `int`
 
     // Prompt user for plaintext
     string text = get_string("plaintext: ");
+    string cipher = text;
 
     // For each character in the plaintext:
     for (int i = 0; i < strlen(text); i++)
     {
-        // Rotate the character if it's a letter
-        // if (text[i] ==  char) {}
+        cipher[i] = rotate(text[i], key);
     }
 
-    printf("cyphertext: %s\n", text);
+    printf("ciphertext: %s\n", text);
 
     return 0;
+}
+
+bool only_digits(string key)
+{
+    for (int i = 0; i < strlen(key); i++)
+    {
+        if (isdigit(key[i]) == 0)
+        {
+            printf("Usage: ./caesar key\n");
+            return false;
+        }
+    }
+    return true;
+}
+
+
+char rotate(char letter, int key)
+{
+    char c = letter;
+    if (isalpha(letter))
+    {
+        if (islower(letter))
+        {
+            if (letter + (key % 26) > 122)
+            {
+                c = '`' + (letter + (key % 26) - 'z');
+            } else
+            {
+                c = letter + (key % 26);
+            }
+        }
+
+        if (isupper(letter))
+        {
+            if (letter + (key % 26) > 90)
+            {
+                c = '@' + (letter + (key % 26) - 'Z');
+            } else
+            {
+                c = letter + (key % 26);
+            }
+        }
+    }
+        // if (isdigit(letter) || ispunct(letter)) {}
+
+    return c;
 }
