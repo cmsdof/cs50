509cfb4ea4f42f834e15d7b4c8d647c700d37b9a
Carlos Severo <carlosmseverof@gmail.com>
2026-06-26 18:22:21 -0300

    Update pointers.c

--- lecture4-pointers/pointers.c
+++ lecture4-pointers/pointers.c
@@ -3,5 +3,17 @@
 int main (void)
 {
     int n = 50;
+
+    // Declaring a Pointer 
+    // The asterisk (*) defines the variable as a pointer to that data type
+    // Examples: int*, float*, struct*
+    int* p = &n;
+
+    // Assigning an variable's adress to a Pointer
+    // The ampersand (&) make so that the adress of the variable is assigned insted of the value
+    p = &n;
+
+    printf("%i\n", n);
     printf("%p\n", &n);
+
 }
